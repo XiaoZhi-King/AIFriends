ff920e3dd1d79aa04a663d29aae5df894baa70b4
河中第一深情 <2746541792@qq.com>
2026-06-16 09:33:13 +0800

    补丁

--- fronted/src/components/character/chat_field/ChatField.vue
+++ fronted/src/components/character/chat_field/ChatField.vue
@@ -47,8 +47,7 @@ function handlePushFrontMessage(msg){
 
 }
 
-function handeelClose(){
-  modalRef.value.close()
+function handleClose(){
   inputRef.value.close()
 }
 
@@ -58,9 +57,9 @@ defineExpose({
 </script>
 
 <template>
-  <dialog ref="modal-ref" class="modal">
+  <dialog ref="modal-ref" class="modal" @close="handleClose()">
     <div class="modal-box w-90 h-150" :style="modalStyle">
-      <button @click="handeelClose()" class="btn btn-sm btn-circle btn-ghost bg-transparent absolute right-1 top-1">×</button>
+      <button @click="modalRef.close()" class="btn btn-sm btn-circle btn-ghost bg-transparent absolute right-1 top-1">×</button>
       <ChatHistory
           ref="chat-history-ref"
           v-if="friend"
